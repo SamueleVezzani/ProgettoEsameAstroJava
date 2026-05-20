2b4d16fc83f24335e2a6df6b01d0fade173a532e
SamueleVezzani <samuvezzani21@gmail.com>
2026-05-20 11:18:18 +0200

    Creazione GameLoop

--- src/main/java/org/example/progettoesameastrojava/GameEngine/GameLoop.java
+++ src/main/java/org/example/progettoesameastrojava/GameEngine/GameLoop.java
@@ -1,8 +1,37 @@
 package org.example.progettoesameastrojava.GameEngine;
 
 import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.GraphicsContext;
+import org.example.progettoesameastrojava.Entities.GameMap;
+import org.example.progettoesameastrojava.Entities.Player;
+
+import static com.sun.scenario.effect.impl.prism.PrEffectHelper.render;
+import static jdk.jfr.internal.consumer.EventLog.update;
 
 public class GameLoop extends AnimationTimer{
     private GameMap map;
-    private Player player
+    private Player player;
+    private GraphicsContext graphicsContext;
+
+    public GameLoop(GraphicsContext graphicsContext){
+        this.graphicsContext=graphicsContext;
+        this.map=new GameMap();
+        this.player=new Player();
+    }
+
+    @Override
+    public void handle(long now) {
+        update();
+        render();
+    }
+
+    private void update(){
+        player.update(map);
+    }
+
+    private void render() {
+        graphicsContext.clearRect(0, 0, 800, 600);
+        map.render(graphicsContext);
+        player.render(graphicsContext);
+    }
 }
