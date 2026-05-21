b6b194685a57e23393aeac7c2fa478bcc47bdeb1
SamueleVezzani <samuvezzani21@gmail.com>
2026-05-21 22:03:34 +0200

    Introduzioni classi parte grafica

---
+++ src/main/java/org/example/progettoesameastrojava/gestionegenerale/Launcher.java
@@ -0,0 +1,12 @@
+package org.example.progettoesameastrojava.gestionegenerale;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+}
---
+++ src/main/java/org/example/progettoesameastrojava/schermatevisive/GameScreen.java
@@ -0,0 +1,4 @@
+package org.example.progettoesameastrojava.schermatevisive;
+
+public class GameScreen {
+}
