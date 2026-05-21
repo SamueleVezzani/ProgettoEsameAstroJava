79a0f6bf85c74f765ab12324bcae80b71718e375
SamueleVezzani <samuvezzani21@gmail.com>
2026-05-21 22:41:14 +0200

    gestione generale 1

--- src/main/java/org/example/progettoesameastrojava/gestionegenerale/Launcher.java
+++ src/main/java/org/example/progettoesameastrojava/gestionegenerale/Launcher.java
@@ -9,4 +9,30 @@ import javafx.stage.Stage;
 
 public class Launcher extends Application {
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        SceneManager sm=new SceneManager(stage);
+        sm.switchToMenu();
+    }
+    public static VBox createMenu(SceneManager sm){
+        VBox menu=new VBox(20);
+        Button btnStart=new Button("inizia");
+        Button btnOptions=new Button("opzioni");
+        Button btnExit=new Button("esci");
+
+        menu.setAlignment(Pos.CENTER);
+        menu.getChildren().addAll(btnStart,btnOptions,btnExit);
+
+        btnStart.setOnAction(e->sm.switchToGame());
+
+        btnOptions.setOnAction(e->sm.switchToMenu());
+
+        btnExit.setOnAction(e->sm.switchToGameOver());
+
+        return menu;
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
 }
