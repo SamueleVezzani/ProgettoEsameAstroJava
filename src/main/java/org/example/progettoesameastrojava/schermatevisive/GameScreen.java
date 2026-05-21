57099823b43c1fbed98cf621ee3200019c244b5d
fo457 <fo4572412@gmail.com>
2026-05-21 22:38:33 +0200

    aggiunta metodi e attributi delle schermate

--- src/main/java/org/example/progettoesameastrojava/schermatevisive/GameScreen.java
+++ src/main/java/org/example/progettoesameastrojava/schermatevisive/GameScreen.java
@@ -1,4 +1,34 @@
 package org.example.progettoesameastrojava.schermatevisive;
 
+import javafx.geometry.Pos;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
 public class GameScreen {
+    //attributi
+    private BorderPane rootLayout;
+    private Canvas gameCanvas;
+    private Label lblScore;
+    private Label lblLives;
+    //metodi
+    public GameScreen(){
+        //metto il canvas al centro e creo l'HUD in alto
+        BorderPane.setAlignment(gameCanvas, Pos.CENTER);
+        HBox barra = new HBox(10);
+        barra.getChildren().addAll(lblScore, lblLives);
+        BorderPane.setAlignment(barra,Pos.TOP_CENTER);
+    }
+    public BorderPane getLayout(){
+        return rootLayout;
+    }
+    public Canvas getCanvas(){
+        return gameCanvas;
+    }
+    public void updateHUD(int score, int lives){
+        this.lblScore.setText(String.valueOf(score));
+        this.lblLives.setText(String.valueOf(lives));
+    }
+
 }
--- src/main/java/org/example/progettoesameastrojava/schermatevisive/MenuScreen.java
+++ src/main/java/org/example/progettoesameastrojava/schermatevisive/MenuScreen.java
@@ -1,4 +1,22 @@
 package org.example.progettoesameastrojava.schermatevisive;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import org.example.progettoesameastrojava.gestionegenerale.SceneManager;
+
 
 public class MenuScreen {
+    //attributi
+    private VBox rootLayout;
+    private Button btnStart;
+
+    public MenuScreen(SceneManager sm){
+        //implementazione dei bottoni
+        //switchToMenu
+        //switchToGame
+        //switchToGameOver
+
+    }
+    public VBox getLayout(){
+        return rootLayout;
+    }
 }
