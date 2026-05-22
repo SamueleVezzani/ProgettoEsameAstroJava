fc3a4e15da419b5bc52602d0ef511852b31ee628
fo457 <fo4572412@gmail.com>
2026-05-22 17:22:36 +0200

    implementazine render

--- src/main/java/org/example/progettoesameastrojava/GameEngine/AssetManager.java
+++ src/main/java/org/example/progettoesameastrojava/GameEngine/AssetManager.java
@@ -1,4 +1,30 @@
 package org.example.progettoesameastrojava.GameEngine;
 
+import javafx.scene.image.Image;
+import java.util.HashMap;
+import java.util.Objects;
+
 public class AssetManager {
+    //attributi
+    private static HashMap <String, Image> sprites;
+    //metodi
+    public static void loadAssets(){
+        //utilizzare
+        //loadImage("<nome sprite>","/resources/images/<sprite.png>");
+
+    }
+    private static void loadImage(String key, String path) {
+        try {
+            Image img = new Image(Objects.requireNonNull(AssetManager.class.getResourceAsStream(path)));
+            sprites.put(key, img);
+        } catch (Exception e) {
+            System.err.println("Impossibile caricare l'immagine: " + path);
+        }
+    }
+
+    public Image getImage(String key){
+        return sprites.get(key);
+    }
+
+
 }
